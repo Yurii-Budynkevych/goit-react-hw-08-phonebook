19220c123009a505e4154af7201f88b9ab5bff69
Yurii-Budynkevych <yurii.budynkevych@gmail.com>
2022-11-15 23:40:12 +0200

    tip add

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,17 @@ import { configureStore } from '@reduxjs/toolkit';
 import { contactsSlice } from './contactsSlice';
 import { filterSlice } from './filterSlice';
 
+// const ownLogger = store => {
+//   return next => {
+//     return action => {
+//       const prevState = store.getState();
+//       console.log(prevState);
+//       console.log(action);
+//       next(action);
+//     };
+//   };
+// };
+
 export const store = configureStore({
   reducer: {
     contacts: contactsSlice.reducer,
