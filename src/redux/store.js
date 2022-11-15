3ce9f926733b8a661fa60391cff186b8ae7b5b98
Yurii-Budynkevych <yurii.budynkevych@gmail.com>
2022-11-15 21:36:52 +0200

    redux-persist remove

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,39 +1,11 @@
-import { configureStore, combineReducers } from '@reduxjs/toolkit';
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+import { configureStore } from '@reduxjs/toolkit';
 import { contactsSlice } from './contactsSlice';
 import { filterSlice } from './filterSlice';
 
-const persistConfig = {
-  key: 'savedContacts',
-  storage,
-  whitelist: ['contacts'],
-};
-
-const persistedReducer = persistReducer(
-  persistConfig,
-  combineReducers({
+export const store = configureStore({
+  reducer: {
     contacts: contactsSlice.reducer,
     filter: filterSlice.reducer,
-  })
-);
-
-export const store = configureStore({
-  reducer: persistedReducer,
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
+  },
+  middleware: [],
 });
-export const persistor = persistStore(store);
